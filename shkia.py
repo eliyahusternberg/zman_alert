590232deaff42bcc76647f3eebf8e27f32a88076
Eliyahu Sternberg <eliyahusternberg@gmail.com>
2018-07-31 22:15:50 -0400

    first commit

---
+++ shkia.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def get_shkia(zipcode):
+    url = 'http://www.hebcal.com/shabbat/?cfg=json&geo=zip&zip=11691&b=0&m=72'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    from pprint import pprint
+    items_list = data['items']
+
+    for item in items_list:
+        if 'Candle lighting' in item['title']:
+            string1 = item['title']
+            print(string1)
+
+            # pprint(data)
+
+            string2 = string1.split(' ')
+            print(string2)
+            shkia = string2[2]
+            print(shkia)
+            print('Shkia this week is at {}'.format(shkia))
---
+++ text_messages.py
@@ -0,0 +1,16 @@
+import telesign
+from telesign.messaging import MessagingClient
+
+
+def send_text(mobil_phone,msg):
+    def send_text_to_user(html: object, alert: object):
+        customer_id = "5822EFA3-0D31-4D97-A6B8-BB7A09F865FD"
+        api_key = "ESqaCPvHsGh7V4513hFMf/+Y3BG1Zl/7Gt5irGhmfi/cMcI7DHwh2lIJwhyLMXThd54q6oWckRq5sZ/NeJo+zQ=="
+
+        phone_number = "15163128958"
+        message = "Shkia is at..."
+        message_type = "ARN"
+
+        messaging = MessagingClient(customer_id, api_key)
+        response = messaging.message(phone_number, message, message_type)
+        print(response, messaging.message(phone_number, message, message_type))
