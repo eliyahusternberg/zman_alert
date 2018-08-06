462d7d147dcab8c9f1fceaba000fe4ba5bdc9f4b
eliyahusternberg <33271054+eliyahusternberg@users.noreply.github.com>
2018-08-06 08:38:55 -0400

    update

--- text_messages.py
+++ text_messages.py
@@ -1,6 +1,8 @@
 import telesign
 from telesign.messaging import MessagingClient
-import shkia
+from shkia import get_shkia
+import schedule
+import time
 
 
 def send_text(mobil_phone,msg):
@@ -9,9 +11,18 @@ def send_text(mobil_phone,msg):
         api_key = "ESqaCPvHsGh7V4513hFMf/+Y3BG1Zl/7Gt5irGhmfi/cMcI7DHwh2lIJwhyLMXThd54q6oWckRq5sZ/NeJo+zQ=="
 
         phone_number = "15163128958"
-        message = "Shkia is at..."
+        message = get_shkia.message
         message_type = "ARN"
 
         messaging = MessagingClient(customer_id, api_key)
         response = messaging.message(phone_number, message, message_type)
         print(response, messaging.message(phone_number, message, message_type))
+
+        
+        
+ def set_weekley_reminder():
+    reminder =schedule.every().friday
+
+while True:
+    schedule.run_pending()
+    time.sleep(.001)
