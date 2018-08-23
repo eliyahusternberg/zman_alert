0199d911b3df848d6b98e123c55343e82f551c1c
eliyahusternberg <33271054+eliyahusternberg@users.noreply.github.com>
2018-08-23 00:27:15 -0400

    update

--- text_messages.py
+++ text_messages.py
@@ -5,24 +5,21 @@ import schedule
 import time
 
 
-def send_text(mobil_phone,msg):
-    def send_text_to_user(html: object, alert: object):
-        customer_id = "5822EFA3-0D31-4D97-A6B8-BB7A09F865FD"
-        api_key = "ESqaCPvHsGh7V4513hFMf/+Y3BG1Zl/7Gt5irGhmfi/cMcI7DHwh2lIJwhyLMXThd54q6oWckRq5sZ/NeJo+zQ=="
+def send_text(mobile_phone, msg):
+    customer_id = "5822EFA3-0D31-4D97-A6B8-BB7A09F865FD"
+    api_key = "ESqaCPvHsGh7V4513hFMf/+Y3BG1Zl/7Gt5irGhmfi/cMcI7DHwh2lIJwhyLMXThd54q6oWckRq5sZ/NeJo+zQ=="
 
-        phone_number = "15163128958"
-        message = get_shkia.message
-        message_type = "ARN"
+    phone_number = mobile_phone
+    message = msg
+    message_type = "ARN"
 
-        messaging = MessagingClient(customer_id, api_key)
-        response = messaging.message(phone_number, message, message_type)
-        print(response, messaging.message(phone_number, message, message_type))
+    messaging = MessagingClient(customer_id, api_key)
+    response = messaging.message(phone_number, message, message_type)
+    print(response, messaging.message(phone_number, message, message_type))
 
-        
-        
- def set_weekley_reminder():
-    reminder =schedule.every().friday
 
+schedule.every().sunday().do(send_text,'17189868004','shkia is at {}'.format(shkia))
 while True:
     schedule.run_pending()
-    time.sleep(.001)
+    time.sleep(1)
+send_text(input('Enter your phone number:'), ('Shkia is at {}'.format(get_shkia(input('Enter zipcode')))))
