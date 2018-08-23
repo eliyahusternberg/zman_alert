9e9692f110bb09a360fb995e3e140a2200473d53
eliyahusternberg <33271054+eliyahusternberg@users.noreply.github.com>
2018-08-23 00:34:06 -0400

    update

--- shkia.py
+++ shkia.py
@@ -2,28 +2,23 @@ import requests
 import json
 
 
-
-
 def get_shkia(zipcode):
     url = 'http://www.hebcal.com/shabbat/''?cfg=json&geo=zip&zip={}&b=0&m=72'.format(zipcode)
     response = requests.get(url)
     data = json.loads(response.text)
     from pprint import pprint
     items_list = data['items']
-
     for item in items_list:
         if 'Candle lighting' in item['title']:
             string1 = item['title']
-            print(string1)
+            #print(string1)
 
             # pprint(data)
 
             string2 = string1.split(' ')
-            print(string2)
+            #print(string2)
             shkia = string2[2]
-            print(shkia)
-            print('Shkia this week is at {}'.format(shkia))
+            #print(shkia)
+
 
     return shkia
-
-zip= get_shkia(input('Enter a zipcode:'))
